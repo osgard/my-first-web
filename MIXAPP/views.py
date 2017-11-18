964903437d952f0f86eeb25792054fa985c979b8
osgard <osgard_kn@hotmail.com>
2017-11-18 16:16:53 -0600

    modificamos templates para mostrar diferentes post edsde la base de datos

--- MIXAPP/views.py
+++ MIXAPP/views.py
@@ -2,7 +2,12 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Musica#Musica es el modelo/tabla que creamos
+#en el tutorial se llama Post
 
 # Create your views here.
 def disc_list(request):
-    return render(request, 'MIXAPP/disc_list.html', {})
+   # Muscia = Musica.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    Muscia = Musica.objects.order_by('created_date')
+    return render(request, 'MIXAPP/disc_list.html', {'Muscia':Muscia})
